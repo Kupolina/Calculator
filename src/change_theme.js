6a8f968075e4dba90b9ec13af2c3eb160e798810
Kulik Polina <kulikpolina.dev@gmail.com>
2022-12-20 22:29:54 +0300

    Some code style changes

--- src/change_theme.js
+++ src/change_theme.js
@@ -1,12 +1,11 @@
 export default document.querySelector('.calc__theme').onclick = function () {
-    document.querySelector(".calc").classList.toggle('white');
-    document.querySelector(".calc__theme").classList.toggle('white');
-    document.querySelector(".calc__screen p").classList.toggle('white');
-    document.querySelector(".theme__changer").classList.toggle('white');
+  document.querySelector('.calc').classList.toggle('white');
+  document.querySelector('.calc__theme').classList.toggle('white');
+  document.querySelector('.calc__screen p').classList.toggle('white');
+  document.querySelector('.theme__changer').classList.toggle('white');
 
-    let buttonList = document.querySelectorAll(".calc__button");
-    for(let i = 0; i < buttonList.length; i++){
-        buttonList[i].classList.toggle('white');
-    }
-}
-
+  const buttonList = document.querySelectorAll('.calc__button');
+  for (let i = 0; i < buttonList.length; i += 1) {
+    buttonList[i].classList.toggle('white');
+  }
+};
